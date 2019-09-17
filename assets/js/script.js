3f7d1fe820d1493e12f722e9df45a499282883c3
fuadguluzade <44554892+fuadguluzade@users.noreply.github.com>
2019-09-17 14:49:58 -0700

    added card columns

---
+++ assets/js/script.js
@@ -0,0 +1,81 @@
+// --- GENRE OPTIONS ---
+// Action, Adventure, Animation, Comedy, Crime, Documentary, Drama, Family, Fantasy, History, 
+// Horror, Music, Mystery, Romance, Science, Fiction, TV Movie, Thriller, War, Western
+
+// IDEAS:
+
+// Happinness = Comedy
+// Angry = War,Crime
+// Contempt = War
+// accessories? = Music
+// Sadness = Romantic
+// Surpise = Thriller
+// Age > 50 = History
+// Fear = Horror
+// Glasses, Facial Hair = Science
+// Age < 16 = Animation
+// Neutral = documentary
+// Bald = War
+
+var titles = [];
+var poster = [];
+var overviews = [];
+
+let genre1 = "Thriller";
+let genre2 = "Comedy";
+
+var genreSettings = {
+    "async": true,
+    "crossDomain": true,
+    "url": "https://api.themoviedb.org/3/genre/movie/list?language=en-US&api_key=9c78553cb3547d0c21d49df380da12a6",
+    "method": "GET",
+    "headers": {},
+    "data": "{}"
+}
+
+$.ajax(genreSettings).done(function (response) {
+    for (let i = 0; i < response.genres.length; i++) {
+        if (genre1 == response.genres[i].name) {
+            genre1 = response.genres[i].id;
+        } else if (genre2 == response.genres[i].name) {
+            genre2 = response.genres[i].id;
+        }
+    }
+    console.log(response)
+
+    var movieSettings = {
+        "async": true,
+        "crossDomain": true,
+        "url": `https://api.themoviedb.org/3/discover/movie?with_genres=${genre1},${genre2}&page=1&include_video=false&include_adult=false&sort_by=popularity.desc&language=en-US&api_key=9c78553cb3547d0c21d49df380da12a6`,
+        "method": "GET",
+        "headers": {},
+        "data": "{}"
+    }
+
+    $.ajax(movieSettings).done(function (response) {
+        console.log(response);
+        for (let i = 0; i < response.results.length; i++) {
+            overviews.push(response.results[i].overview)
+            titles.push(response.results[i].title);
+            poster.push(`https://image.tmdb.org/t/p/original${response.results[i].poster_path}`);
+            $('#movies').append(getCardItem(i));
+        }
+    });
+
+});
+
+const getCardItem = function (index) {
+    var $overlay = $(`<div class = "view overlay"</div>`);
+    var $cardItem = $(`<div class="card mr-3" style="width: 15rem;"></div>`);
+    var $image = $(`<img class="card-img-top gif mb-3 img-fluid" src=${poster[index]}>`);
+    $overlay.append($image);
+    var $text = $(`<div class="mask flex-center rgba-blue-strong w-auto h-auto overflow-auto"></div>`);
+    $text.append(`<p class="white-text bg-transparent">${overviews[index]}</p>`);
+    $overlay.append($text);
+    var $cardBody = $(`<div class="card-body"></div>`);
+    $cardBody.append($overlay);
+    $cardBody.append(`<h5 class="card-title">${titles[index]}</h5>`);
+    $cardItem.append($cardBody);
+
+    return $cardItem;
+};
