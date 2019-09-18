8a2ec04d96c1b3e49456a434b28c2f46a7b80993
Weston Carpenter <wc.cc96@gmail.com>
2019-09-18 10:20:04 -0700

    integrated face api into layout

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,20 @@
 let faceData;
+let img_url;
+
+$('#submit').click(function () {
+
+    img_url = $('#imageURL').val();
+    $('.imageHolder').html(`<img src="${img_url}" class="img-thumbnail shadow p-3 mb-4 bg-white rounded"
+    alt="Responsive image" id="image"></img>`);
+    processImage(img_url);
+
+});
+
+$('#fileButton').on('click', openDialog);
+
+function openDialog() {
+    img_url = $('#file').click();
+}
 
 function processImage(source_url) {
     const face_api_key = "1164a87de384422aaa3ca0e1ee7c6f3d";
@@ -23,11 +39,12 @@ function processImage(source_url) {
         method: "POST",
         data: '{"url": ' + '"' + source_url + '"}',
     })
-    .then(function (data) {
-        console.log(data);
-        faceData = data;
-    })
-    .fail(function (error) {
-        console.log(error);
-    });
+        .then(function (data) {
+            $('#char').append(`<h1>Age : ${data[0].faceAttributes.age}</h1>`);
+            console.log(data);
+            faceData = data;
+        })
+        .fail(function (error) {
+            console.log(error);
+        });
 };
